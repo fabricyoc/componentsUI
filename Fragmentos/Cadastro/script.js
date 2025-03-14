809800321a6d5e15e22e3ed966138bf7ee287656
fabricyo <fazin_c@hotmail.com>
2025-03-14 09:32:40 -0300

    Formulário

---
+++ Fragmentos/Cadastro/script.js
@@ -0,0 +1,16 @@
+// Obtém a data atual no formato YYYY-MM-DD
+const dataHoje = new Date().toISOString().split("T")[0];
+
+// Define o atributo 'max' do input para a data de hoje
+document.getElementById("dataNasc").setAttribute("max", dataHoje);
+
+// Seleciona todos os inputs da página
+const inputs = document.querySelectorAll("input");
+inputs.forEach(
+    input => {
+        input.addEventListener("blur", () => {
+            input.value === "" ? input.classList.add("inputNaoPreenchido") : input.classList.remove("inputNaoPreenchido");
+        });
+    }
+);
+
